5fb7b6f5eb7f3a788e0012e522ff6a6f5e9faf91
Lain <ckisielius@gmail.com>
2024-11-14 17:29:06 -0500

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,40 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+import App from "./App";
+import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import ContactMe from "./pages/ContactMe";
+import Resume from "./pages/Resume";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NoMatch />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/contactMe",
+        element: <ContactMe />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
